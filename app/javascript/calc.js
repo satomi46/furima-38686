3ab8ab14248829e935703f10b6938e973deec591
satomi46 <satohat@gmail.com>
2022-11-18 15:35:48 +0900

    JavaScriptによる手数料と利益の表示機能の追加

---
+++ app/javascript/calc.js
@@ -0,0 +1,21 @@
+function post () {
+  console.log("イベント発火")
+
+  const price_breakdown = document.getElementById("item-price")
+  price_breakdown.addEventListener("input", function(){
+    const total_price = document.getElementById("item-price").value;
+    const tax_price_with_decimal = total_price * 0.1
+    const tax_price = Math.floor(tax_price_with_decimal).toLocaleString();
+    const margin_with_decimal = total_price * 0.9
+    const margin = Math.floor(margin_with_decimal).toLocaleString();
+
+    const add_tax_price = document.getElementById("add-tax-price")
+    const profit = document.getElementById("profit")
+    // console.log(total_price)
+    // console.log(sales_fee, sales_margin)
+    add_tax_price.innerHTML = tax_price + " "
+    profit.innerHTML = margin + " "
+  });
+};
+
+window.addEventListener('load', post);
