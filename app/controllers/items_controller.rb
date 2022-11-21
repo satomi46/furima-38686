6959582c73f3ab501697193eea0c53b2c94e5f62
satomi46 <satohat@gmail.com>
2022-11-21 11:04:08 +0900

    Rubocopの実行と指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     if @item.update(item_params)
       redirect_to item_path(params[:id])
@@ -46,9 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
-      redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
   end
-
 end
