871e1be5f99d0b6e597c5f4272ce48c5b180fbe5
satomi46 <satohat@gmail.com>
2022-11-21 22:08:04 +0900

    購入画面への遷移、購入画面へのitemテーブルデータの反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :birthday, presence: true
 
   has_many :items
+  has_many :purchases
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
